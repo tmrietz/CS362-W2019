d4c024f154b1c18cc48793dd1de05af245c8427b
tmrietz <tmrietz@gmail.com>
2019-03-13 18:56:06 +0100

    Add methods for random testing by length

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -1,6 +1,7 @@
 import junit.framework.TestCase;
 import java.util.Scanner;
 import java.util.Random;
+import org.apache.commons.lang3.RandomStringUtils;
 
 //You can use this as a skeleton for your 3 different test approach
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
@@ -24,7 +25,7 @@ public class UrlValidatorTest extends TestCase {
 		private String validSchemeSeparator = "://";
 		private String validPortSeparator = ":";
 		private String validDomainSeparator = ".";
-		private String validPathSeparator = "/";
+		private String validPathChars = "//////////////////////////////.-_~!$&'()*+,;=";		// 66/33 probability of '/' for more realistic paths
 		private String validFragmentSeparator = "#";
 		private String invalidSeparators = "<>\\~{}\"";
 		private String[] someValidSubDomains = {"test","www","any","string","0r","numer1c","is--alright, sup3r"};
@@ -92,9 +93,8 @@ public class UrlValidatorTest extends TestCase {
 	           "http://www.google.com/.../world",
 	           "http://www.google.com/hell",
 	           "http://example.com/serach?address=Main%20Avenue",
-	           "http://www.google.com/file.c",
-	           
-	   };
+						 "http://www.google.com/file.c"
+			};
 	   
 	   // This list of URLs are bad -- failures here mean that the application failed to report the failure
 	   String[] shouldNotWork = {
@@ -117,9 +117,7 @@ public class UrlValidatorTest extends TestCase {
 			   "http://www.google.com/#/file",
 			   "http://www.google.com/#",
 			   "http://www.google.com:-1",
-			   "http://www.google.com:65a",
-
-			   
+			   "http://www.google.com:65a"
 	   };
 	   
 	   System.out.println("URLs that should work but errors out");
@@ -132,7 +130,65 @@ public class UrlValidatorTest extends TestCase {
 		   myAssert(shouldNotWork[i], urlVal.isValid(shouldNotWork[i]), false);
 	   }
 	   
-   }
+	 }
+	 
+
+	 public void testRandomLengthPartition() {
+
+		// set up url lengths (tested in ranges) for testing steps
+		int[] urlLengths = {
+			0, 
+			50,
+			100,
+			1000,
+			10000,
+			100000
+			//1000000,
+			//10000000
+		};
+
+		// initialize our validator
+		UrlValidator urlVal = new UrlValidator (
+			null, 
+			null,
+			UrlValidator.ALLOW_2_SLASHES +
+			UrlValidator.ALLOW_ALL_SCHEMES +
+			UrlValidator.NO_FRAGMENTS + 
+			UrlValidator.ALLOW_LOCAL_URLS
+		);
+
+		// do some more setup
+		Random randInt = new Random();
+		int positiveTestedPerRangeCount = 0;
+		int negativeTestedPerRangeCount = 0;
+		boolean valid;
+		int rangesTested = 0;											  			// 0-10, 10-100, etc.
+		String url = "";
+
+		// while not all ranges yet tested, generate urls and test for that length range
+		while (rangesTested < urlLengths.length - 1) {
+			int length = randInt.nextInt(urlLengths[rangesTested+1]);
+			if (rangesTested == 0) {
+				myAssert(url, urlVal.isValid(""), false);			// an empty string is not a URL
+				rangesTested++;
+			} else if (length >= 25) {
+				valid = randInt.nextBoolean();
+				url = randGenerateUrlOfLength(false, valid, length);
+				myAssert(url, urlVal.isValid(url), valid);
+				if (valid) {
+					positiveTestedPerRangeCount++;
+				} else {
+					negativeTestedPerRangeCount++;
+				}
+
+				if (positiveTestedPerRangeCount > 3 && negativeTestedPerRangeCount > 3) {
+					rangesTested++;
+					positiveTestedPerRangeCount = 0;
+					negativeTestedPerRangeCount = 0;
+				}
+			}
+		}
+	}
    
    
    public void testYourFirstPartition()
@@ -362,7 +418,197 @@ public class UrlValidatorTest extends TestCase {
 	   System.out.println("********************************\n");
 	   
 	 }
-	 
+
+/***************************************Start Rand Testing Functions***************************************/
+
+
+
+
+	public String randGenerateUrlOfLength(boolean changeOrder, boolean valid, int length) {
+		// set up some randomization to determine whether url will be valid
+		if (length <= 10) return "";
+		Random rand = new Random();
+		int remainingLength = length;
+
+		// set *semi-random* lengths for each url part
+		int protocolLength = 7;
+		remainingLength -= protocolLength;
+		int domainLength = 0;
+		while (domainLength < 25) {
+			rand.nextInt(remainingLength);
+		}
+		int domainLength = rand.nextInt(remainingLength);
+		remainingLength -= domainLength;
+		int fragmentLength = remainingLength/(rand.nextInt(3) + 1);
+		remainingLength -= fragmentLength;
+		int pathLength = remainingLength/(rand.nextInt(3) + 1);		// don't divide by zero
+		remainingLength -= pathLength;
+		int queryLength = remainingLength;
+
+
+		// create individual url parts
+		String[] resultParts = new String[length];
+		resultParts[0] = "http://";																// we may wish to make the protocol random but we'll get faults
+		resultParts[1] = randDomainOfLength(domainLength, valid);
+		resultParts[2] = randPathOfLength(pathLength, valid);
+		resultParts[3] = randQueryOfLength(queryLength, valid);
+		resultParts[4] = randFragmentOfLength(fragmentLength, valid);
+
+		// make an order swap if requested in the function call
+		int order[] = {0,1,2,3,4};
+		String resultStr = "";
+		if (changeOrder) {
+			int tmp = rand.nextInt(4);
+			order[tmp+1] = tmp;
+			order[tmp] = tmp+1;
+		}
+
+		// concatenate together the url string
+		for(int i=0; i<order.length; i++) {
+			resultStr += resultParts[order[i]];
+		}
+		return resultStr;
+	}
+
+	public String randDomainOfLength(int length, boolean valid) {
+		if (length <= 15) return "";										// see below for the hard-coded 3
+		Random randInt = new Random();
+		boolean useIP = randInt.nextBoolean();				// choose whether to use an IP for domain
+		int lengthRemaining = length;
+		if (useIP) {
+			System.out.println("USING IP");							// TO DO: create IP generator
+			return "";
+		} else {
+			// create domain parts, decrementing length along the way
+			String subdomain = randStringFromArray(this.someValidSubDomains, randInt);
+			lengthRemaining -= subdomain.length();
+			String topLevelDomain = randStringFromArray(this.someValidTLDs, randInt);
+			lengthRemaining -= topLevelDomain.length();
+			String domain = RandomStringUtils.randomAlphanumeric(lengthRemaining-3, lengthRemaining-2);		// -3 for separators
+			
+			// use the created domain parts to build a string
+			StringBuilder result = new StringBuilder(subdomain);
+			result.append(this.validDomainSeparator);
+			result.append(domain);
+			result.append(this.validDomainSeparator);
+			result.append(topLevelDomain);
+			result.append("/");
+			
+			// if not valid, invalidate the domain string with bad characters
+			if (! valid) {
+				int invalidCharCount = randInt.nextInt(length/2);
+				while (invalidCharCount > 0) {
+					result.setCharAt(randInt.nextInt(length), randCharFromString(this.invalidSeparators, randInt));
+					invalidCharCount--;
+				}
+			}
+			return result.toString();
+		}
+
+	}
+
+	public String randPathOfLength(int length, boolean valid) {
+		if (length <= 2) return "";
+		Random randInt = new Random();
+		String str = RandomStringUtils.randomAlphanumeric(length, length+1);			//create a random alphanumeric string
+		int numSeparators = randInt.nextInt(length/2);														//get a random number of separators
+		StringBuilder result = new StringBuilder(str);														//use this for easier char replacement
+		
+		while (numSeparators > 0) {
+			// use valid characters for a valid path, else use randomized invalid chars
+			if (valid) {
+				result.setCharAt(randInt.nextInt(length), randCharFromString(this.validPathChars, randInt));			//may need to change to just '/' ...
+			} else {
+				result.setCharAt(randInt.nextInt(length), randCharFromString(this.invalidSeparators, randInt));
+			}
+			numSeparators--;
+		}
+		return result.toString();
+	}
+
+	public String randQueryOfLength(int length, boolean valid) {
+		if (length <= 2) return "";
+		Random randInt = new Random();
+		char[] queryChars = setRandomQueryChars(valid);
+		int numQueryChars = randInt.nextInt(length/2);
+		String str = RandomStringUtils.randomAlphanumeric(length, length+1);
+		StringBuilder result = new StringBuilder(str);
+		result.setCharAt(0, '?');			// set the first character of query to '?' whether valid or invalid
+
+		// don't overwrite the '?', and randomly emplace the other query control chars
+		while (numQueryChars > 0) {
+			int index = randInt.nextInt(length);
+			if (index != 0) {
+				result.setCharAt(index, queryChars[randInt.nextInt(2)]);
+			}	
+			numQueryChars--;
+		}
+		return result.toString();
+	}
+
+	public String randFragmentOfLength(int length, boolean valid) {
+		if (length <= 0) return "";
+		Random randInt = new Random();
+		String str = RandomStringUtils.randomAlphanumeric(length, length+1);			//create a random alphanumeric string
+		StringBuilder result = new StringBuilder(str);
+		result.setCharAt(0, this.validFragmentSeparator.charAt(0));					// use '#' whether valid or invalid
+		
+		// We don't *guarantee* here that the number of invalid chars matches invalidCharCount because
+		// the main point is just to invalidate the string in a randomized fashion.
+		if (! valid) {
+			int invalidCharCount = randInt.nextInt(length);
+			while (invalidCharCount > 0) {
+				int index = randInt.nextInt(length);
+				if (index != 0)
+					result.setCharAt(randInt.nextInt(length), randCharFromString(this.invalidSeparators, randInt));
+				invalidCharCount--;
+			}
+		}
+		return result.toString();
+	}
+
+	public char randCharFromString(String inputStr, Random rand) {
+		return inputStr.charAt(rand.nextInt(inputStr.length()));
+	}
+
+	public String randStringFromArray(String[] inputStrings, Random rand) {
+		return inputStrings[rand.nextInt(inputStrings.length)];
+	}
+
+	public char[] setRandomQueryChars(boolean valid) {
+		Random randInt = new Random();
+		char[] queryChars = {'\0','\0'};
+		
+		// str contains either valid or invalid chars
+		String str = this.validQuerySeparators;
+		if (! valid) {
+			str = this.invalidSeparators;
+		}
+		queryChars[0] = randCharFromString(str, randInt);
+
+		// use the valid or invalid chars, and get 2 of them (distinct)
+		char nextChar;
+		while (true) {
+			nextChar = randCharFromString(str, randInt);
+			if (nextChar != queryChars[0]) {
+				queryChars[1] = randCharFromString(str, randInt);
+				break;
+			}
+		}
+		return queryChars;
+	}
+
+	public boolean intInArray(int[] arr, int testInt) {
+		for(int i=0; i<arr.length; i++) {
+			if (arr[i] == testInt)
+				return true;
+		}
+		return false;
+	}
+
+
+/*****************************************End Rand Testing Functions***************************************/
+
 
    private boolean customAssertEquals(boolean val1, boolean val2)
    {
